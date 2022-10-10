19f4e86fe9cca578003ed5b680ddac62b7d3dfde
Marceloslife <113509452+Marceloslife@users.noreply.github.com>
2022-10-10 15:12:40 +0700

    Pengembangan android 2 layout
    pertemuan youtube

---
+++ app/src/main/java/com/si5a/belajar2laout/MainActivity.java
@@ -0,0 +1,26 @@
+package com.si5a.belajar2laout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+ Button btnPindah;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPindah = findViewById(R.id.btn_pindah);
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this, secondactivity.class);
+                startActivity(pindah);
+            }
+        });
+    }
+}
